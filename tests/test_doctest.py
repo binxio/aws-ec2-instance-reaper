6c961aca9de4a0efdf1679dcffef9394a375af55
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2024-04-25 13:03:06 +0200

    initial build

---
+++ tests/test_doctest.py
@@ -0,0 +1,7 @@
+# from durations import Duration
+# from datetime import datetime, timedelta
+# def test_humanize():
+#     x = Duration("10 days")
+#     y = (datetime.utcnow() + timedelta(seconds=7200))
+#     z = Duration(y.isoformat())
+#     print(x,y, z)
