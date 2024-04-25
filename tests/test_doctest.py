36bc61e2884d0fbd7f5851e84c3710cf664127e9
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2024-04-25 15:33:00 +0200

    chore: remove unused tests

--- tests/test_doctest.py
+++
@@ -1,7 +0,0 @@
-# from durations import Duration
-# from datetime import datetime, timedelta
-# def test_humanize():
-#     x = Duration("10 days")
-#     y = (datetime.utcnow() + timedelta(seconds=7200))
-#     z = Duration(y.isoformat())
-#     print(x,y, z)
